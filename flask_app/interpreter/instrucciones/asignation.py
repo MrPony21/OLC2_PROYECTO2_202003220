0e64672ba2ac2770065b307422fa8214cd88300f
MrPony21 <79233577+MrPony21@users.noreply.github.com>
2024-04-20 01:10:00 -0600

    while update

--- flask_app/interpreter/instrucciones/asignation.py
+++ flask_app/interpreter/instrucciones/asignation.py
@@ -5,6 +5,7 @@ from ..entorno.symbol import Symbol
 from ..entorno.types import Type
 from ..entorno.asmbol import Asmbol
 from ..entorno.generator import Generator
+from ..entorno.asmvar import Asmvar
 
 class Asignation(Instruccion):
     def __init__(self, line, column,identificador, valor: Expression, tipo_asignacion):
@@ -48,13 +49,59 @@ class Asignation(Instruccion):
             new_valor = valor_variable.value - exp.value
             env.changeVariable(out, self.identificador, exp.type, new_valor, self.line, self.column)
         
-    def generateASM(self, out, env: Enviroment, generator):
+    def generateASM(self, out, env: Enviroment, generator: Generator):
         
         exp: Asmbol = self.valor.generateASM(out, env, generator)
+        print("entre en la asginacionsdkfjal")
+        #obtendremos la variable del enviroment 
+        var: Asmvar = env.getVariableASM(self.identificador)
 
         if self.tipo_asignacion == "=":
             
+            generator.add_br()
+            #primero obtendremos el nuevo valor
+            generator.add_li('t3', str(exp.valuePos))
+            generator.add_lw('t1', '0(t3)')
+            
+            #ahora lo cargaremos en la anterior posicion
+            generator.add_li('t4', str(var.valuepos))
+            generator.add_sw('t1', '0(t4)')
 
-            env.changeVariableASM(self.identificador, exp.type, exp.valuePos, exp.value)
+
+            env.changeVariableASM(self.identificador, var.type, var.valuepos, exp.value)
 
         #faltan las asignaciones de otros tipos
+        elif self.tipo_asignacion == "+=":
+
+            generator.add_br()
+            #primero obtendremos el nuevo valor
+            generator.add_li('t3', str(exp.valuePos))
+            generator.add_lw('t1', '0(t3)')
+            generator.add_li('t3', str(var.valuepos))
+            generator.add_lw('t2', '0(t3)')
+            generator.add_operation('add', 't0', 't1', 't2')
+
+            #ahora lo cargaremos en la anterior posicion
+            generator.add_li('t4', str(var.valuepos))
+            generator.add_sw('t0', '0(t4)')
+
+            env.changeVariableASM(self.identificador, var.type, var.valuepos, exp.value)
+
+        elif self.tipo_asignacion == "-=":
+            
+            print("hola")
+            generator.add_br()
+            #primero obtendremos el nuevo valor
+            generator.add_li('t3', str(var.valuepos))
+            generator.add_lw('t1', '0(t3)')
+            generator.add_li('t3', str(exp.valuePos))
+            generator.add_lw('t2', '0(t3)')
+            generator.add_operation('sub', 't0', 't1', 't2')
+
+            #ahora lo cargaremos en la anterior posicion
+            generator.add_li('t4', str(var.valuepos))
+            generator.add_sw('t0', '0(t4)')
+
+            env.changeVariableASM(self.identificador, var.type, var.valuepos, exp.value)
+
+
